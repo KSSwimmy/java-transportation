d16611e6dfcea779f6839b727b98f2246e90e254
Lambda_School_Loaner_163 <kimberlyswinton@gmail.com>
2019-09-04 15:12:17 -0400

    :tada: initial commit
    initial commit

---
+++ transportation-2/src/main/java/com/lambdaschool/transport/Animal.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.transport;
+
+public interface Animal
+{
+}
---
+++ transportation-2/src/main/java/com/lambdaschool/transport/TransportApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.transport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TransportApplication.class, args);
+    }
+
+}
